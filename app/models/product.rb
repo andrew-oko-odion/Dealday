2443e6304f7b67068b2304662e8a882e41388e48
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-04-18 21:29:34 +0100

    Add validation for form attributes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :color, presence: true
+  validates :sold_out, presence: true
 end
