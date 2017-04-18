75cd95ddf92b65215555e93f07c1dc2bf4c6f3eb
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-04-18 15:00:27 +0100

    Add update and create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,51 @@
 class ProductsController < ApplicationController
-
   def index
+    @products = Product.all 
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new 
+  end
+  
+  def create
+    add_product = Product.new(product_params)
+    if add_product.save
+      flash[:notice] = "Product saved successfully"
+      redirect_to products_path
+    else
+      flash[:notice] = "Product Not created"
+      render 'new'
+    end
+  end
+  
   def edit
+    @product = Product.find(params[:id])
   end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+      redirect_to product_path(@product)
+      flash[:notice] = "Product updated successfully"
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+  end
+  
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private 
   def product_params
     params.require(:product).permit(:name, :category, :sold_out, :color)
   end
